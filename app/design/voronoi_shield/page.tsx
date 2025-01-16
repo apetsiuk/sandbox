adc86e13b64f279a8cd824f4e46cb356bfd0da1f
Alex Petsiuk <aliaksei.petsiuk@gmail.com>
2025-01-16 17:48:35 -0500

    upd

--- app/design/voronoi_shield/page.tsx
+++ app/design/voronoi_shield/page.tsx
@@ -1,6 +1,4 @@
 
-import Link from 'next/link';
-
 
 export default function Voronoi_shield_Page() {
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Link from 'next/link';
 import logoImg from '@/assets/ap_logo.jpg';
 
